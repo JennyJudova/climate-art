75f9c57b939571d8885ca280c5b8760ba35e42d4
JennyJudova <jjudova@gmail.com>
2020-11-18 19:23:32 +0000

    chore: add to extinction array

--- app/routes/404.js
+++ app/routes/404.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class fourOFourRoute extends Route {
   model() {
-    return ['West African Black Rhinoceros', 'Baiji White Dolphin', 'Pyrenean Ibex', 'Passenger Pigeon', 'Tasmanian Tiger', 'Stellers Sea Cow', 'Great Auk', 'Dodo', 'Woolly Mammoth', 'Sabre-toothed Cat', 'Lamb-succory', 'Downy hemp-nettle', 'Cooksonia']
+    return ['West African Black Rhinoceros', 'Baiji White Dolphin', 'Pyrenean Ibex', 'Passenger Pigeon', 'Tasmanian Tiger', 'Stellers Sea Cow', 'Great Auk', 'Dodo', 'Woolly Mammoth', 'Sabre-toothed Cat', 'Lamb-succory', 'Downy hemp-nettle', 'Cooksonia', 'Aral Sea']
   }
 }
