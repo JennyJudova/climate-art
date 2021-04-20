aa16d835c046ddf0efb0ada94fe51b7810caee5b
JennyJudova <jjudova@gmail.com>
2021-04-20 22:46:35 +0100

    chore: update package json

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,15 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    prember: {
-      urls: [
-        '/',
-        '/mission',
-        '/projects',
-        '/past-projects',
-        '/contact'
-      ]
-    }
+
   });
 
   // Use `app.import` to add additional libraries to the generated
